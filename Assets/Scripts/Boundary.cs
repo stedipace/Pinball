d40acc6183a791f65bbb06941a86ad3fe3fd8151
Max <61809127+maxwellpark@users.noreply.github.com>
2024-09-20 22:01:22 +0100

    Change flipper and add boundary gizmo

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[RequireComponent(typeof(EdgeCollider2D))]
+public class Boundary : MonoBehaviour
+{
+    private EdgeCollider2D edgeCollider;
+
+    void Awake()
+    {
+        edgeCollider = GetComponent<EdgeCollider2D>();
+    }
+
+    void OnDrawGizmos()
+    {
+        if (edgeCollider == null || edgeCollider.points.Length < 2)
+            return;
+
+        Gizmos.color = new Color(0, 1, 0, 0.5f);
+
+        var points = edgeCollider.points;
+        var previousPoint = transform.TransformPoint(points[0]);
+
+        for (int i = 1; i < points.Length; i++)
+        {
+            var currentPoint = transform.TransformPoint(points[i]);
+            Gizmos.DrawLine(previousPoint, currentPoint);
+            previousPoint = currentPoint;
+        }
+    }
+}
