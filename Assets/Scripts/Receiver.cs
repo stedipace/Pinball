2dbfcac572966ea7d952c7adf30ca334cf1578d8
Max <61809127+maxwellpark@users.noreply.github.com>
2024-09-21 17:24:26 +0100

    Generic animation play on receiver

--- Assets/Scripts/Receiver.cs
+++ Assets/Scripts/Receiver.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public abstract class ReceiverBase : MonoBehaviour
 {
     [SerializeField] private float waitTime = 0.5f;
+    [SerializeField] private string animationName;
 
     protected virtual void OnEnter(Collider2D collision)
     {
@@ -21,6 +22,13 @@ public abstract class ReceiverBase : MonoBehaviour
             var trail = ballRb.GetComponent<TrailRenderer>();
             trail.Clear();
         }
+
+        if (animationName != null)
+        {
+            var animator = collision.GetComponentInChildren<Animator>(true);
+            animator.gameObject.SetActive(true);
+            animator.Play(animationName);
+        }
     }
 
     protected virtual void OnExit(Collider2D collision)
@@ -30,6 +38,12 @@ public abstract class ReceiverBase : MonoBehaviour
             ballRb.isKinematic = false;
             ballRb.velocity = Vector2.zero;
             ballRb.angularVelocity = 0f;
+        }
+
+        if (animationName != null)
+        {
+            var animator = collision.GetComponentInChildren<Animator>();
+            animator.gameObject.SetActive(false);
         }
     }
 
--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -4,21 +4,9 @@ public class Teleporter : ReceiverBase
 {
     [SerializeField] private Transform destination;
 
-    protected override void OnEnter(Collider2D collision)
-    {
-        base.OnEnter(collision);
-
-        var animator = collision.GetComponentInChildren<Animator>(true);
-        animator.gameObject.SetActive(true);
-        animator.Play("Teleport");
-    }
-
     protected override void OnExit(Collider2D collision)
     {
         base.OnExit(collision);
-
-        var animator = collision.GetComponentInChildren<Animator>();
-        animator.gameObject.SetActive(false);
         collision.transform.position = destination.position;
     }
 }
