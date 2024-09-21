ad7e8603feb3104f8cc972c2ac123dad0861cf6e
Max <61809127+maxwellpark@users.noreply.github.com>
2024-09-21 11:29:10 +0100

    Visual feedback for kicker

--- Assets/Scripts/Kicker.cs
+++ Assets/Scripts/Kicker.cs
@@ -1,8 +1,22 @@
+using System.Collections;
 using UnityEngine;
 
 public class Kicker : MonoBehaviour
 {
     [SerializeField] private float force = 10f;
+    [SerializeField] private Color hitColor = Color.red;
+    [SerializeField] private SpriteRenderer kickerEdgeSprite;
+    [SerializeField] private float colorFlashDuration = 0.2f;
+
+    private Color originalColor;
+
+    private void Start()
+    {
+        if (kickerEdgeSprite != null)
+        {
+            originalColor = kickerEdgeSprite.color;
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -16,5 +30,29 @@ public class Kicker : MonoBehaviour
             var direction = (collision.transform.position - transform.position).normalized;
             ballRb.AddForce(direction * force, ForceMode2D.Impulse);
         }
+
+        if (kickerEdgeSprite != null)
+        {
+            StartCoroutine(FlashColor());
+        }
+
+        //ContactPoint2D contact = collision.GetContact(0);
+        //if (IsCollisionOnKickerEdge(contact.point))
+        //{
+
+        //}
+    }
+
+    //private bool IsCollisionOnKickerEdge(Vector2 collisionPoint)
+    //{
+    //    var edgePosition = kickerEdgeSprite.transform.position;
+    //    return collisionPoint.x > edgePosition.x;
+    //}
+
+    private IEnumerator FlashColor()
+    {
+        kickerEdgeSprite.color = hitColor;
+        yield return new WaitForSeconds(colorFlashDuration);
+        kickerEdgeSprite.color = originalColor;
     }
 }
